5c618ba28769c341787eb8985ee63b99300437c1
梁惠涌 <1026946613@qq.com>
2016-04-20 19:36:47 +0800

    增加数据传输

--- app/src/main/java/com/tyky/adb/Service/ADBService.java
+++ app/src/main/java/com/tyky/adb/Service/ADBService.java
@@ -2,11 +2,18 @@ package com.tyky.adb.Service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+import com.tyky.adb.Utils.DataUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -17,10 +24,10 @@ import java.util.concurrent.Executors;
 public class ADBService extends Service {
 
     private String TAG = "ADBService";
-    int SERVER_PORT = 19986;
+    final int SERVER_PORT = 19986;
     volatile boolean isRun = false;
-    private BufferedInputStream in;
-    private BufferedOutputStream out;
+    private DataOutputStream date_Out;
+    private DataInputStream data_In;
 
     private ServerSocket serverSocket = null;
     private Socket client;
@@ -30,6 +37,7 @@ public class ADBService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -44,25 +52,76 @@ public class ADBService extends Service {
             @Override
             public void run() {
                 try {
+                    Log.e(TAG,"客户端已开启");
                     //创建服务器Socket
                     serverSocket = new ServerSocket();
                     serverSocket.bind(new InetSocketAddress("127.0.0.1", Port));
                     client = serverSocket.accept();
 
                     if(client != null){
-                        out = new BufferedOutputStream(client.getOutputStream());
-                        in = new BufferedInputStream(client.getInputStream());
+                        data_In = new DataInputStream(client.getInputStream());
+                        date_Out = new DataOutputStream(client.getOutputStream());
                         Log.e(TAG,"客户端已连接");
-                    }
 
+                        //图片位置
+                        String path=Environment.getExternalStorageDirectory().getAbsolutePath()+"/DCIM/Camera/IMG_20160410_000155.jpg";
+                        Log.e(TAG,path);
+                        sendImg(date_Out,path);
+
+                        //发送数据
+                        isRun = true;
+                        //while (isRun){
+                            sendImg(date_Out,path);
+                            Thread.sleep(3000);
+                        //}
+                    }else {//断开连接
+                        Log.e(TAG,"断开连接");
+                        return;
+                    }
                 } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         });
     }
 
+    //以byte数组 发送图片给客户端
+    void sendImg(DataOutputStream out,String path) throws IOException {
 
+        //获取图片file
+        FileInputStream file = new FileInputStream(path);
+        int imgSize = file.available(); //图片的长度
+
+        //传过去的byte数组的总长
+        //byte[] btLength = DataUtils.intToByte(imgSize+4+4);
+
+        //打flag 1:字符串 2：图片
+        byte[] btFlag = DataUtils.intToByte(2);
+
+        //将图片file转成byte数组
+        byte[] btImg = new byte[imgSize];
+        file.read(btImg);
+        file.close();
+
+        Log.e(TAG,"btImg.length = "+btImg.length);
+
+        //拼接要发送的byte数组
+        byte[] sendData = DataUtils.byteMerger(btFlag,btImg);
+
+        //发送byte给客户端
+        //out.writeInt(imgSize);
+        out.write(sendData);
+        out.flush();
+    }
+
+    //接收客户端发来的图片
+    void recvimg(DataInputStream in, Socket client){
+
+    }
+
+    //
     void sendStr(String str){
 
     }
@@ -76,7 +135,14 @@ public class ADBService extends Service {
 
     @Override
     public void onDestroy() {
-        isRun = false;
+        //关闭
+        try {
+            isRun = false;
+            serverSocket.close();
+            Log.e(TAG,"关闭服务器");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         super.onDestroy();
     }
 }
